2bc00c25c872c2312858abaa879f969efe96155f
Ilya Abramov <45449706+Yushint@users.noreply.github.com>
2022-02-10 17:12:16 +0300

    Седьмой коммит. Разработка класса дневника.
    Создан класс DiaryWindow, наследуемый от базового класса окон Window. Данный класс отвечает за работу непосредственно GUI дневника. 
    Созданы простое меню и подменю. На функции-комманды подменю пока что поставлены "заглушки" (pass).

--- diary.py
+++ diary.py
@@ -1,8 +1,42 @@
 from tkinter import *
 from main_window import Window
 from PIL import Image, ImageTk
+import os
 
 
-app = Window()
+class DiaryWindow(Window):
+    def __init__(self):
+        super().__init__()
+    
+    def set_diary_menu(self):
+        self.mainmenu = Menu(self.root)
+        self.root.configure(menu=self.mainmenu)
+        
+        self.filemenu = Menu(self.mainmenu, tearoff=0)
+        self.filemenu.add_command(label="Экспорт отзывов", command=self.export)
+        self.filemenu.add_separator()
+        self.filemenu.add_command(label="Выход", command=self.exit)
+        self.mainmenu.add_cascade(label="Дневник", menu=self.filemenu)
+        
+        self.helpmenu = Menu(self.mainmenu, tearoff=0)
+        self.helpmenu.add_command(label="Помощь", command=self.help)
+        self.helpmenu.add_command(label="О программе", command=self.about)
+        self.mainmenu.add_cascade(label="Справка", menu=self.helpmenu)
+    
+    def export(self):
+        pass
+    
+    def exit(self):
+        pass
+    
+    def help(self):
+        pass
+    
+    def about(self):
+        pass
+        
+    
+app = DiaryWindow()
 app.set_icon()
+app.set_diary_menu()
 app.run()
