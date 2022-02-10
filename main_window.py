191b19f26d255276a2493282a22425d393dc6e9b
Ilya Abramov <45449706+Yushint@users.noreply.github.com>
2022-02-10 16:21:22 +0300

    Второй коммит. Добавлена фоновая картинка.
    Установлены размер окна, привязка места появления окна к центру экрана. Обновлён фон.

--- main_window.py
+++ main_window.py
@@ -1,7 +1,23 @@
 # is made by Abramov Ilya (@evoriath), GNU GPL
 from tkinter import *
 from PIL import Image, ImageTk
+import os
 
 
 root = Tk()
+
+WIDTH = root.winfo_screenwidth()
+HEIGHT = root.winfo_screenheight()
+WIDTH = WIDTH // 2
+HEIGHT = HEIGHT // 2
+WIDTH = WIDTH - 200
+HEIGHT = HEIGHT - 200
+
+image = Image.open("img/background.jpg")
+tk_image = ImageTk.PhotoImage(image)
+label = Label(width=650, height=200, image=tk_image)
+label.pack()
+
+
+root.geometry('650x350+{}+{}'.format(WIDTH, HEIGHT))
 root.mainloop()
