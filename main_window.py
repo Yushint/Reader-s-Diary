000e806bcf85100c38298f1d6f46bc9931c0b159
Ilya Abramov <45449706+Yushint@users.noreply.github.com>
2022-02-10 16:23:11 +0300

    Третий коммит. Создан класс MainWindow.
    Класс MainWindow отвечает за логику всего главного окна. Добавлена кнопка входа в читательский дневник.

--- main_window.py
+++ main_window.py
@@ -4,20 +4,59 @@ from PIL import Image, ImageTk
 import os
 
 
-root = Tk()
+class MainWindow:
+    def __init__(self):
+            self.root = Tk()
+            self.WIDTH = self.root.winfo_screenwidth()
+            self.HEIGHT = self.root.winfo_screenheight()
+            self.WIDTH = self.WIDTH // 2
+            self.HEIGHT = self.HEIGHT // 2
+            self.WIDTH = self.WIDTH - 200
+            self.HEIGHT = self.HEIGHT - 200
+            WIDTH, HEIGHT = self.WIDTH, self.HEIGHT
+            self.root.geometry('650x350+{}+{}'.format(self.WIDTH, self.HEIGHT))
+            self.root.title("Reader's Diary")
+            self.root.resizable(False, False)
+            self.root.config(bg="#f5f5dc")
 
-WIDTH = root.winfo_screenwidth()
-HEIGHT = root.winfo_screenheight()
-WIDTH = WIDTH // 2
-HEIGHT = HEIGHT // 2
-WIDTH = WIDTH - 200
-HEIGHT = HEIGHT - 200
+    def set_frames(self):
+        self.top_frame = Frame(self.root)
+        self.bottom_frame = Frame(self.root)
+        self.top_frame.pack(side=TOP)
+        self.bottom_frame.pack(side=TOP)
 
-image = Image.open("img/background.jpg")
-tk_image = ImageTk.PhotoImage(image)
-label = Label(width=650, height=200, image=tk_image)
-label.pack()
+    def set_background_image(self):
+        self.background_image = Image.open("img/background.jpg")
+        self.tk_background_image = ImageTk.PhotoImage(self.background_image)
+        self.top_label.config(image=self.tk_background_image)
+
+    def set_labels(self):
+        self.top_label = Label(self.top_frame, width=650, height=190, bg="white")
+        #self.bottom_canvas = Canvas(self.bottom_frame, bg="#f5f5dc", width=650, height=200)
+        self.top_label.pack(side=TOP)
+        #self.bottom_canvas.pack(side=BOTTOM)
+
+    def set_enter_button(self):
+        self.button = Button(self.bottom_frame, text="Reader's Diary", font=("Courier New", 20), command=self.enter, bg="#f5f5dc", activebackground="#f5f5dc")
+        self.button.pack(side=TOP)
+
+    def enter(self):
+        self.root.destroy()
+        os.system("python diary.py")
+
+    def set_icon(self):
+        self.icon_img = Image.open("img/icon.jpg")
+        self.icon_img.save("Icon.ico")
+        self.root.iconbitmap("Icon.ico")
+
+    def run(self):
+        self.root.mainloop()
 
 
-root.geometry('650x350+{}+{}'.format(WIDTH, HEIGHT))
-root.mainloop()
+window = MainWindow()
+window.set_frames()
+window.set_labels()
+window.set_enter_button()
+window.set_background_image()
+window.set_icon()
+window.run()
